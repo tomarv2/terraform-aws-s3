1ff6197248e04f57fae4e35847cb51d8cc98ae34
tomarv2 <varuntomar2019@gmail.com>
2021-03-31 23:48:04 -0700

    adding flag to bucket public block

--- s3.tf
+++ s3.tf
@@ -22,11 +22,11 @@ resource "aws_s3_bucket" "bucket" {
 resource "aws_s3_bucket_public_access_block" "root_storage_bucket" {
   count = var.deploy_bucket ? 1 : 0
 
-  bucket              = join("", aws_s3_bucket.bucket.*.id)
-  ignore_public_acls  = true
-  depends_on          = [aws_s3_bucket.bucket]
-  block_public_acls   = true
-  block_public_policy = true
+  bucket                  = join("", aws_s3_bucket.bucket.*.id)
+  ignore_public_acls      = true
+  depends_on              = [aws_s3_bucket.bucket]
+  block_public_acls       = true
+  block_public_policy     = true
   restrict_public_buckets = true
 }
 
